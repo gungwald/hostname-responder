bf9a4b42802ecee099dbf4b53f3c00479c7453fa
Bill Chatfield <bill_chatfield@yahoo.com>
2026-05-01 11:58:35 -0400

    Linux install improvements

--- src/install.sh
+++ src/install.sh
@@ -6,30 +6,53 @@ PGM=hwaet
 PGMD="$PGM"d
 INSTALL_DIR='/usr/local'
 DAEMON_NAME='Hostname Responder Daemon'
+# DAEMON_USER is system specific: OpenBSD has an underscore prepended
 
 installSystemdService()
 (
+    DAEMON_USER="$1"
     SVC="$PGMD.service"
+    INSTALLED_SVC_DIR=/usr/lib/systemd/system
     if [ "$OP" = "install" ]
     then
-	# If no group is given, it should create a group for the user.
-        useradd -c "$DAEMON_NAME" -d / -s /sbin/nologin "$DAEMON_USER"
-	# Copy pre-built service file into the system directory.
-        install -p -o root -g root -m u=rwx,g=rx,o=rx "$SVC" /etc/systemd/system
-	# Recognize the new service file
-	systemctl daemon-reload
+        # If no group is given, it should create a group for the user.
+        useradd --comment "$DAEMON_NAME" --no-create-home \
+            --home-dir /var/empty \
+            --shell /sbin/nologin "$DAEMON_USER" 
+        # Copy pre-built service file into the system directory.
+        install -p -o root -g root -m u=rwx,g=rx,o=rx "$SVC" \
+            "$INSTALLED_SVC_DIR"
+        # Recognize the new service file
+        systemctl daemon-reload
         systemctl enable "$SVC"
         systemctl start "$SVC"
-	systemctl status "$SVC"
+        systemctl status "$SVC"
     else
-        systemctl stop "$SVC"
-        systemctl disable "$SVC"
-        rm /etc/systemd/system/"$SVC"
+        systemctl stop "$SVC" || exit
+        systemctl disable "$SVC" || exit
+        rm "$INSTALLED_SVC_DIR"/"$SVC" || exit
         userdel $DAEMON_USER
+        # Remove mail spool file but not home directory. Doing this with
+        # "userdel --remove" would remove the home directory also, but
+        # it is a shared directory in this case. So don't do that.
+        MAIL_DIR=`grep '^MAIL_DIR' /etc/login.defs | cut -f2`
+        rm "$MAIL_DIR"
     fi
 )
 
-if [ "$1" = "-u" ]
+#########################
+#
+# Main script starts here
+#
+#########################
+
+if [ `whoami` != 'root' ]
+then
+    echo You must be root for this to work. 1>&2
+    exit 1
+fi
+
+if [ "$1" = "-u" ] || [ "$1" = "uninstall" ] || [ "$1" = "--uninstall" ]
 then
     OP=uninstall
 else
@@ -42,12 +65,12 @@ case $OS in
         DAEMON_USER="_$PGMD"
         if [ "$OP" = "install" ]
         then
-	    install -sp -o root -g bin $PGM $INSTALL_DIR/bin
-	    install -sp -o root -g bin $PGMD $INSTALL_DIR/sbin
-	    install -p -o root -g wheel -m a=rx $PGMD.rc.sh /etc/rc.d/$PGMD
-            # Loosely based on info from there: https://www.maklin.fi/post/sysadmin/2023-10-22_how-to-add-services-on-openbsd/
-            useradd -g =uid -c "$DAEMON_NAME" -L daemon -d /var/empty -s /sbin/nologin "$DAEMON_USER"
-	    rcctl set $PGMD user $DAEMON_USER
+            install -sp -o root -g bin $PGM $INSTALL_DIR/bin
+            install -sp -o root -g bin $PGMD $INSTALL_DIR/sbin
+            install -p -o root -g wheel -m a=rx $PGMD.rc.sh /etc/rc.d/$PGMD
+            useradd -g =uid -c "$DAEMON_NAME" -L daemon -d /var/empty -s \
+                /sbin/nologin "$DAEMON_USER" # Ok if user exists
+            rcctl set $PGMD user $DAEMON_USER
             rcctl enable $PGMD
             rcctl start $PGMD
         else
@@ -61,16 +84,22 @@ case $OS in
     FreeBSD | NetBSD)
         echo Don\'t know how on $OS. Please write me.
         exit
-        
+    ;;
+    
+    # Mac
     Darwin)
         # This won't work. It needs to be modified for Mac.
         if [ "$OP" = "install" ]
         then
-	    install -sp -o root -g bin $PGM $INSTALL_DIR/bin
-	    install -sp -o root -g bin $PGMD $INSTALL_DIR/sbin
-	    install -p -o root -g wheel -m a=rx $PGMD.rc.sh /etc/rc.d/$PGMD
-            useradd -g =uid -c "$DAEMON_NAME" -L daemon -d /var/empty -s /sbin/nologin "$DAEMON_USER"
-	    rcctl set $PGMD user $DAEMON_USER
+            install -sp -o root -g bin $PGM $INSTALL_DIR/bin
+            install -sp -o root -g bin $PGMD $INSTALL_DIR/sbin
+            install -p -o root -g wheel -m a=rx $PGMD.rc.sh /etc/rc.d/$PGMD
+            if grep -q $DAEMON_USER /etc/passwd
+            then
+                useradd -g =uid -c "$DAEMON_NAME" -L daemon -d /var/empty -s \
+                    /sbin/nologin "$DAEMON_USER"
+            fi
+            rcctl set $PGMD user $DAEMON_USER
             rcctl enable $PGMD
             rcctl start $PGMD
         else
@@ -91,15 +120,22 @@ case $OS in
             rm $INSTALL_DIR/bin/$PGM $INSTALL_DIR/sbin/$PGMD 
         fi
         
-        if type systemd
+        if type systemctl >/dev/null 2>&1
         then
-            installSystemdService
-        elif type rcctl
+            installSystemdService "$DAEMON_USER"
+        elif type rcctl >/dev/null 2>&1
+        then
             if [ "$OP" = "install" ]
             then
                 install -p -o root -g wheel -m a=rx $PGMD.rc.sh /etc/rc.d/$PGMD
-                useradd -c "$DAEMON_NAME" -d / -s /sbin/nologin "$DAEMON_USER"
-	        rcctl set $PGMD user $DAEMON_USER
+                if grep -q $DAEMON_USER /etc/passwd
+                then
+                    echo User $DAEMON_USER already exists.
+                else
+                    useradd -c "$DAEMON_NAME" -d / -s /sbin/nologin \
+                        "$DAEMON_USER"
+                fi
+                rcctl set $PGMD user $DAEMON_USER
                 rcctl enable $PGMD
                 rcctl start $PGMD
             else
