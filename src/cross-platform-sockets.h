2fe100587b961fd14ff437bf2e221918aa1e2b36
Bill Chatfield <bill_chatfield@yahoo.com>
2026-05-25 00:38:25 -0400

    Fix run-time errors

--- src/cross-platform-sockets.h
+++ src/cross-platform-sockets.h
@@ -1,6 +1,12 @@
 #ifndef CROSS_PLATFORM_SOCKETS_H
 #define CROSS_PLATFORM_SOCKETS_H
 
+/* Bind, the standard internet name server software, claims that RFC883 claims
+   that a Fully Qualified Domain Name (FQDN) can be 1025 bytes. This doesn't
+   seem correct, as every other source says it's 255 bytes. But, it is a safe
+   number because it's the biggest option. See /usr/include/arpa/nameser.h. */
+#define FQDN_MAX_LEN 1025
+
 /* Just the string with no terminator, useful in for stmt comparisons */
 #define ETH_ADDR_STR_LEN 17
 /* ff:FF:FF:FF:FF:FF plus string terminator char */
@@ -25,24 +31,27 @@ typedef struct sockaddr_in6 sain6;  /* type sain6 = struct sockaddr_in6 */
    Posted by user4200092, modified by community. See post 'Timeline' for change history
    Retrieved 2026-04-23, License - CC BY-SA 3.0 */
 #ifdef _WIN32
-    /* See http://stackoverflow.com/questions/12765743/getaddrinfo-on-win32 */
-    #include <winsock2.h>
-    #include <Ws2tcpip.h>
-    #pragma comment(lib, "ws2_32.lib")
-    typedef int socklen_t;
+  /* See http://stackoverflow.com/questions/12765743/getaddrinfo-on-win32 */
+  #include <winsock2.h>
+  #include <Ws2tcpip.h>
+  #pragma comment(lib, "ws2_32.lib")
+  typedef int socklen_t;
 #else
-    #include <sys/socket.h> /* socket, bind, getifaddrs, freeifaddrs */
-    #include <sys/types.h>  /* getifaddrs, freeifaddrs */
-    #include <netinet/in.h> /* sockaddr_in, in_port_t, INET_ADDRSTRLEN, INET6_ADDRSTRLEN */
-    #include <netinet/if_ether.h> /* ether_aton on BSD */
-    #include <netinet/ether.h> /* ether_aton on Linux */
-    #include <arpa/inet.h>  /* inet_ntop */
-    #include <net/if.h>	    /* IFF_BROADCAST */
-    #include <ifaddrs.h>    /* getifaddrs, freeifaddrs, ifaddrs */
-    #include <limits.h>     /* POSIX HOST_NAME_MAX */
-    #include <unistd.h>
-    #define SOCKET int
-    #define INVALID_SOCKET -1
+  #include <sys/socket.h>       /* socket, bind, getifaddrs, freeifaddrs */
+  #include <sys/types.h>        /* getifaddrs, freeifaddrs */
+  #include <net/if_arp.h>       /* Required for types in netinet/if_ether.h */
+  #include <netinet/in.h>       /* sockaddr_in, in_port_t, INET_ADDRSTRLEN, INET6_ADDRSTRLEN */
+  #include <netinet/if_ether.h> /* ether_aton on BSD */
+#ifdef __linux__
+  #include <netinet/ether.h>    /* Linux: defines ether_aton, BSD: file nonexistant */
+#endif
+  #include <arpa/inet.h>        /* inet_ntop */
+  #include <net/if.h>	        /* IFF_BROADCAST */
+  #include <ifaddrs.h>          /* getifaddrs, freeifaddrs, ifaddrs */
+  #include <limits.h>           /* POSIX HOST_NAME_MAX */
+  #include <unistd.h>
+  #define SOCKET int
+  #define INVALID_SOCKET -1
 #endif
 
 
--- src/wake-on-lan.c
+++ src/wake-on-lan.c
@@ -19,14 +19,19 @@ struct WakeOnLan
   uint8_t tgtEthAddrX16[96];
 };
 
-char *chomp(/*out*/ char *s);
+char *reduceToFirstColumn(/* in out */ char *s);
+char *chomp(/*in out*/ char *s);
 bool isEthAddrStr(const char *hexEthAddr);
 void initWakeOnLan(/*out*/ struct WakeOnLan *w, const struct ether_addr *target);
-void initBroadcastAddress(/*out*/ struct sockaddr_in *bcAddr, int port);
-bool sendWakeOnLan(const struct ether_addr *target, const char *tgtHexAddr);
-void wakeAllInFile(const char *fName);
+bool initBroadcastAddress(/*out*/ struct sockaddr_in *bcAddr, const char *bcAddrStr, int port);
+bool sendWakeOnLan(const struct ether_addr *target, const char *tgtHexAddr, const char *hn);
+void wakeAllInFile(const char *fileName);
 void handleError(const char *msg, const char *entity);
-void handleAppError(const char *msg, const char *entity, const char *entity2);
+void handleError2(const char *msg, const char *entity, const char *entity2);
+void handleAppError(const char *msg, const char *entity);
+void handleAppError2(const char *msg, const char *entity, const char *entity2);
+bool isCompleteLine(const char *line);
+char *parseLine(const char *line, char *addr, size_t addrSz, char *hn, size_t hnSz);
 
 
 char *pgm;
@@ -40,45 +45,114 @@ int main(int argc, char *argv[])
   pgm = argv[0];
   stat = EXIT_SUCCESS;
   
-  if (argc > 1) {
-    for (i = 1; i < argc; i++) {
-      /* Check for ethernet mac address on cmd line before file name. */
-      sleeper = ether_aton(argv[i]); /* Tests if it is an ethernet address. */
-      if (sleeper != NULL)
-        sendWakeOnLan(sleeper, argv[i]);
+  if (argc > 1)
+    for (i = 1; i < argc; i++)
+      if ((sleeper = ether_aton(argv[i])) != NULL)
+        sendWakeOnLan(sleeper, argv[i], "<UNKNOWN>");
       else
       	wakeAllInFile(argv[i]);
-    }
-  } else
+  else
     wakeAllInFile("/etc/ethers");
   return stat;
 }
 
-char *chomp(char *s)
+/**
+ * If the buffer for gets is too short to read the full line, only a partial
+ * line will be read. Then the next read will continue reading the same line.
+ * But it will read half a line. This will completely ruin input that should
+ * have one entry per line. So it is necessary to be able to detect whether
+ * a complete line has been read or not.
+ */
+bool isCompleteLine(const char *line)
+{
+  char lastChar;
+  lastChar = line[strlen(line)-1];
+  return lastChar == '\n' || lastChar == '\r';
+}
+
+char *chompBsd(/* in out */ char *s)
 {
   /* Find first occurance of either \r or \n and put a terminator in it. */
   s[strcspn(s,"\r\n")] = '\0';
   return s;
 }
 
-void wakeAllInFile(const char *fName) {
+char *chompManyCompares(char *s)
+{
+  char *p;
+  p = s;
+  while (*p && *p != '\n' && *p != '\r')
+    p++;
+  *p = '\0'; 
+  return s;
+}
+
+char *chomp(char *s)
+{
+  size_t i;
+  
+  i = strlen(s);
+  while (i > 0 && (s[--i] == '\n' || s[i] == '\r'))
+    s[i] = '\0';
+  return s;
+}
+
+char *reduceToFirstColumn(/* in out */ char *s)
+{
+  s[strcspn(s," \t\r\n")] = '\0'; /* Terminate s after the first token */
+  return s;
+}
+
+char *parseLine(const char *line, char *addr, size_t addrSz, char *hn, size_t hnSz)
+{
+  size_t l = 0;
+  size_t a = 0;
+  size_t h = 0;
+  size_t addrMaxLen;
+  size_t hnMaxLen;
+  
+  addrMaxLen = addrSz - 1;
+  hnMaxLen = hnSz - 1;
+
+  while (line[l] && isspace(line[l]))                     /* Skip whitespace */
+    l++;
+  while (line[l] && a < addrMaxLen && !isspace(line[l]))  /* Copy to addr */
+    addr[a++] = line[l++];
+  while (line[l] && a == addrMaxLen && !isspace(line[l])) /* Skip rest of addr */
+    l++;
+  while (line[l] && isspace(line[l]))                     /* Skip whitespace */
+    l++;
+  while (line[l] && h < addrMaxLen && !isspace(line[l]))  /* Copy to hn */
+    hn[h++] = line[l++];
+
+  addr[a] = '\0';                                          /* Terminate */
+  hn[h] = '\0';
+  return addr;
+}
+
+void wakeAllInFile(const char *fileName)
+{
   FILE *f;
-  char line[ETH_ADDR_LINE_SIZ]; /* Includes line and string terminators. */
+  char line[ETH_ADDR_STR_LEN+1+FQDN_MAX_LEN+3+64]; /* Size includes line and string terminators, plus 64 extra. */
+  char eth[ETH_ADDR_STR_SIZ];
+  char hn[FQDN_MAX_LEN+1];
   struct ether_addr *sleeper;
   
-  if ((f = fopen(fName, "r")) != NULL) {
-    while (fgets(line, sizeof(line), f) != NULL) {
-      if ((sleeper = ether_aton(chomp(line))) != NULL)
-        sendWakeOnLan(sleeper, line);
+  if ((f = fopen(fileName, "r")) != NULL) {
+    while (fgets(line, sizeof(line), f) != NULL)
+      if (isCompleteLine(line))
+        if ((sleeper = ether_aton(parseLine(chomp(line),eth,sizeof(eth),hn,sizeof(hn)))) != NULL)
+          sendWakeOnLan(sleeper, eth, hn);
+        else
+          handleAppError2("not a valid ethernet address", fileName, eth);
       else
-        handleAppError("not a valid ethernet address", fName, line);
-    }
-    if (ferror(f)) /* Check if fgets caused an error, rather than reaching EOF. */
-      handleError("failed to read from file", fName);
-    if (fclose(f) == EOF) /* Close and check for failure. EOF means failure here. */
-      handleError("failed to close file", fName);
+        handleAppError2("ridiculously long line skipped", fileName, line);
+    if (ferror(f)) /* Check required as fgets can stop on EOF or error */
+      handleError("failed to read from file", fileName);
+    if (fclose(f) == EOF) /* EOF means failure here */
+      handleError("failed to close file", fileName);
   } else
-    handleError("failed to open file", fName);
+    handleError("failed to open file", fileName);
 }
 
 /**
@@ -103,8 +177,8 @@ bool isEthAddrStr(const char *s)
 
 void initWakeOnLan(struct WakeOnLan *w, const struct ether_addr *target)
 {
-  const int ETH_ADDR_BYTE_LEN = 6; /* 48-bit eth addr = 6 bytes */
-  const int WOL_ETH_ADDR_REQ_REPETITIONS = 16;
+  const int ETH_ADDR_LEN = 6;       /* 48-bit eth addr = 6 bytes */
+  const int ETH_ADDR_REQ_REPS = 16; /* It must be repeated 16 times */
   
   int tgtIdx=0; /* Must be initialized to zero for loop correctness */
   int srcIdx;   /* Initialized by loop */
@@ -112,59 +186,87 @@ void initWakeOnLan(struct WakeOnLan *w, const struct ether_addr *target)
   
   memset(w->sixMaxValBytes, 0xff, 6); /* 6 repetitions of 255 */
 
-  for (rep = 0; rep < WOL_ETH_ADDR_REQ_REPETITIONS; rep++)
-    for (srcIdx = 0; srcIdx < ETH_ADDR_BYTE_LEN; srcIdx++)
+  for (rep = 0; rep < ETH_ADDR_REQ_REPS; rep++)
+    for (srcIdx = 0; srcIdx < ETH_ADDR_LEN; srcIdx++)
       w->tgtEthAddrX16[tgtIdx++] = target->ether_addr_octet[srcIdx];
 }
 
-void initBroadcastAddress(struct sockaddr_in *bcAddr, int port)
+bool initBroadcastAddress(struct sockaddr_in *addr, const char *withAddr, int withPort)
 {
-  memset(bcAddr, 0, sizeof(struct sockaddr_in));
-  bcAddr->sin_family = AF_INET;
-  /* TODO - Can this fail? */
-  inet_pton(AF_INET, "255.255.255.255", &(bcAddr->sin_addr));
-  bcAddr->sin_port = htons(port);
+  int convResult;
+  bool whetherGoalAchieved = false;
+  
+  memset(addr, 0, sizeof(*addr));
+  addr->sin_family = AF_INET;
+  convResult = inet_pton(AF_INET, withAddr, &(addr->sin_addr));
+  switch (convResult) {
+  case -1:
+    /* System error */
+    handleError("failed to convert broadcast address", withAddr);
+    break;
+  case 0:
+    /* The address to convert is invalid */
+    handleAppError("invalid broadcast address", withAddr);
+    break;
+  case 1:
+    /* Successful conversion */
+    addr->sin_port = htons(withPort);
+    whetherGoalAchieved = true;
+  }
+  return whetherGoalAchieved;
 }
 
-bool sendWakeOnLan(const struct ether_addr *target, const char *tgtHexStr)
+bool sendWakeOnLan(const struct ether_addr *target, const char *tgtHexStr, const char *hn)
 {
-  const int FSYNC_ERR = -1;
+  const char *BROADCAST_ADDR = "192.168.1.255";
+  const int DISCARD_PORT = 9; /* WOL port doesn't matter but discard port is recommended */
   const int CLOSE_ERR = -1;
-  const int BROADCAST_ON = 1;
+  const int ON = 1;           /* As in, to turn the broadcast socket option on. */
   
-  bool isSuccessful = false;
-  struct WakeOnLan *tgtWol;
-  int bcSock;                 /* The broadcast socket */
-  struct sockaddr_in bcAddr;  /* The broadcast address */
+  bool whetherGoalAchieved = false;
+  struct WakeOnLan *wol;
+  int sock;                  /* The broadcast socket */
+  struct sockaddr_in addr;   /* The broadcast address */
   
-  initWakeOnLan(tgtWol, target);
-  initBroadcastAddress(&bcAddr, 0);
-  
-  if ((bcSock = socket(AF_INET, SOCK_DGRAM, 0)) != SOCK_ERR) {
-    if (setsockopt(bcSock, SOL_SOCKET, SO_BROADCAST, &BROADCAST_ON, sizeof(BROADCAST_ON)) != SOCK_ERR)
-      if (sendto(bcSock, tgtWol, sizeof(tgtWol), 0, (sa*) &bcAddr, sizeof(bcAddr)) != SOCK_ERR)
-        isSuccessful = true;
+  printf("Sending WOL for %s (%s)\n", tgtHexStr, hn);
+  initWakeOnLan(wol, target);
+  if (initBroadcastAddress(&addr, BROADCAST_ADDR, DISCARD_PORT))  
+    if ((sock = socket(AF_INET, SOCK_DGRAM, 0)) != SOCK_ERR) {
+      if (setsockopt(sock, SOL_SOCKET, SO_BROADCAST, &ON, sizeof(ON)) != SOCK_ERR)
+        if (sendto(sock, wol, sizeof(wol), 0, (sa*) &addr, sizeof(addr)) != SOCK_ERR)
+          whetherGoalAchieved = true;
+        else
+          handleError2("failed to send wake-on-lan broadcast for", tgtHexStr, hn);
       else
-        handleError("failed to send wake-on-lan broadcast", tgtHexStr);
+        handleError2("failed to set socket to broadcast for", tgtHexStr, hn);
+      if (close(sock) == CLOSE_ERR)
+        handleError2("failed to close wake-on-lan socket for", tgtHexStr, hn);
+    }
     else
-      handleError("failed to set socket to broadcast", tgtHexStr);
-    if (fsync(bcSock) == FSYNC_ERR)
-      handleError("failed to flush wake-on-lan socket", tgtHexStr);
-    if (close(bcSock) == CLOSE_ERR)
-      handleError("failed to close wake-on-lan socket", tgtHexStr);
-  } else
-    handleError("failed to create socket for wake-on-lan", tgtHexStr);
-  return isSuccessful;
+      handleError2("failed to create wake-on-lan socket for", tgtHexStr, hn);
+  return whetherGoalAchieved;
 }
 
 void handleError(const char *msg, const char *entity)
 {
   fprintf(stderr, "%s: %s: %s: %s\n", pgm, msg, entity, strerror(errno));
-  stat = EXIT_FAILURE;
+  stat = EXIT_FAILURE; /* Record global failure */
 }
 
-void handleAppError(const char *msg, const char *entity1, const char *entity2)
+void handleError2(const char *msg, const char *entity, const char *entity2)
+{
+  fprintf(stderr, "%s: %s: %s: %s: %s\n", pgm, msg, entity, entity2, strerror(errno));
+  stat = EXIT_FAILURE; /* Record global failure */
+}
+
+void handleAppError(const char *msg, const char *entity)
+{
+  fprintf(stderr, "%s: %s: %s\n", pgm, msg, entity);
+  stat = EXIT_FAILURE; /* Record global failure */
+}
+
+void handleAppError2(const char *msg, const char *entity1, const char *entity2)
 {
   fprintf(stderr, "%s: %s: %s: %s\n", pgm, msg, entity1, entity2);
-  stat = EXIT_FAILURE;
+  stat = EXIT_FAILURE; /* Record global failure */
 }
